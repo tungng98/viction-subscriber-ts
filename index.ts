7761023fbfda9d136d4a1601e56e07e9882db26a
Tung Nguyen <87560224+tungng98@users.noreply.github.com>
2024-09-20 11:12:45 +0700

    pseudo code for processing tx

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import { BigNumber, ethers } from 'ethers';
 
+const RPC_PROVIDER_URL = 'http://localhost:8545';
+const WS_PROVIDER_URL = 'ws://localhost:8546';
+
 (async function main() {
-	var wsProvider = new ethers.providers.WebSocketProvider('ws://localhost:8546');
+	var wsProvider = new ethers.providers.WebSocketProvider(WS_PROVIDER_URL);
 	wsProvider._websocket.on('open', function() {
 		console.log('Websocket connected');
 	});
@@ -15,7 +18,30 @@ async function newHeadsHandler(head: {[Name: string]: string }) {
 	const blockNumber = BigNumber.from(head['number']);
 	console.log(`New Block: ${head['number']} (${blockNumber.toBigInt()})`);
 
-	const httpProvider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+	const httpProvider = new ethers.providers.JsonRpcProvider(RPC_PROVIDER_URL);
 	const block = await httpProvider.getBlock(head['number']);
 	console.log(`Got Block: ${head['number']} (${blockNumber.toBigInt()}): ${block.hash}`);
+
+	saveTransactions(block.transactions);
+}
+
+async function saveTransactions(txHashes: string[]) {
+	// TODO: Save to DB
+}
+
+async function processTransactionJob() {
+	// TODO: Load from DB
+	const txHashes: string[] = [];
+	for(const txHash of txHashes) {
+		await processTransaction(txHash);
+	}
+}
+
+async function processTransaction(txHash: string) {
+	const httpProvider = new ethers.providers.JsonRpcProvider(RPC_PROVIDER_URL);
+	const txTrace = httpProvider.send('debug_traceTransaction', [txHash, {
+		"tracer":"callTracer",
+		"timeout":"300s"
+	}]);
+	// TODO: process trace result
 }
